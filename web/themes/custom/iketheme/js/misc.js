92daf5473358773d38f5575ef9086a3fbb9dd543
Webster Jorgensen <webster.jorgensen@gmail.com>
2025-01-20 23:20:20 -0600

    ikeducation label update, iked hero link fix, more updates

--- web/themes/custom/iketheme/js/misc.js
+++ web/themes/custom/iketheme/js/misc.js
@@ -1,11 +1,13 @@
-
 document.addEventListener('DOMContentLoaded', () => {
   // Select all anchor tags
   const links = document.querySelectorAll('a');
 
   links.forEach(link => {
-    // Check if the href matches the specified URL
-    if (link.href.includes('https://interland3.donorperfect.net')) {
+    // Check if the href matches the specified URL or ends with .pdf
+    if (
+      link.href.includes('https://interland3.donorperfect.net') ||
+      link.href.endsWith('.pdf')
+    ) {
       link.setAttribute('target', '_blank'); // Set to open in a new tab
       link.setAttribute('rel', 'noopener noreferrer'); // Add security measures
     }
