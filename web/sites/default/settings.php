cc804ce9adf1d20169106734dc5d2e9558c28dcb
Webster Jorgensen <webster.jorgensen@gmail.com>
2025-02-15 02:13:13 -0600

    test

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -882,6 +882,8 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
 
+
+
 // Automatically generated include for settings managed by ddev.
 $ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
 if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
